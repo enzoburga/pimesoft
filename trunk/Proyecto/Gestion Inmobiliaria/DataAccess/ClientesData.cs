1a739277c3847b92101b660a1d35c8505c772d6a
danielledes <danielledes@4ac600be-c34a-0410-9392-7103d457d77e>
2008-04-16 03:19:42 +0000

    Generacion de las clases Cliente y ClientesData

---
+++ trunk/Proyecto/Gestion Inmobiliaria/DataAccess/ClientesData.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GI.DA
+{
+    public class ClientesData
+    {
+        public int Guardar(DateTime FechaNacimiento, string Email, string Apellido, string Nombres, string NroDocumento, string Observaciones, int TelefonoCelular, int TelefonoParticular, int lefonoTrabajo, int tipoDocumento, int IdBarrio, int IdProvincia, string Calle, string CodigoPostal, string Depto, int Numero, string Piso)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public bool Actualizar(int IdCliente, DateTime FechaNacimiento, string Email, string Apellido, string Nombres, string NroDocumento, string Observaciones, int TelefonoCelular, int TelefonoParticular, int lefonoTrabajo, int tipoDocumento, int IdBarrio, int IdProvincia, string Calle, string CodigoPostal, string Depto, int Numero, string Piso)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public bool Eliminar(int IdCliente)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
